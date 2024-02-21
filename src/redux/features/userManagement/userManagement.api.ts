4cb04cc784b05d60513193884e0a19095e89434a
Sunan13208 <sunanrabbi1918@gmail.com>
2024-02-21 17:02:52 +0600

    user management rtk query

---
+++ src/redux/features/userManagement/userManagement.api.ts
@@ -0,0 +1,105 @@
+import { baseApi } from "../../api/baseApi";
+
+const userManagementAPI = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        createSuperAdmin: builder.mutation({
+            query: (data) => ({
+                url: '/user/create-superAdmin',
+                method: "POST",
+                body: data
+            })
+        }),
+        createManager: builder.mutation({
+            query: (data) => ({
+                url: '/user/create-manager',
+                method: "POST",
+                body: data
+            })
+        }),
+        createSeller: builder.mutation({
+            query: (data) => ({
+                url: '/user/create-seller',
+                method: "POST",
+                body: data
+            })
+        }),
+        getAllSuperAdmin: builder.query({
+            query: () => ({
+                url: '/member/getAllSuperAdmin',
+                method: "GET"
+            })
+        }),
+        getAllManager: builder.query({
+            query: () => ({
+                url: '/member/getAllManager',
+                method: "GET"
+            })
+        }),
+        getAllSeller: builder.query({
+            query: () => ({
+                url: '/member/getAllSeller',
+                method: "GET"
+            })
+        }),
+        getSuperAdmin: builder.query({
+            query: (id) => ({
+                url: `/member/getSuperAdmin/${id}`,
+                method: "GET"
+            })
+        }),
+        getManager: builder.query({
+            query: (id) => ({
+                url: `/member/getManager/${id}`,
+                method: "GET"
+            })
+        }),
+        getSeller: builder.query({
+            query: (id) => ({
+                url: `/member/getSeller/${id}`,
+                method: "GET"
+            })
+        }),
+        updateSuperAdmin: builder.mutation({
+            query: (data) => ({
+                url: `/member/updateSuperAdmin/${data.id}`,
+                method: "PUT",
+                body: data.body
+            })
+        }),
+        updateManager: builder.mutation({
+            query: (data) => ({
+                url: `/member/updateManager/${data.id}`,
+                method: "PUT",
+                body: data.body
+            })
+        }),
+        updateSeller: builder.mutation({
+            query: (data) => ({
+                url: `/member/updateSeller/${data.id}`,
+                method: "PUT",
+                body: data.body
+            })
+        }),
+        deleteSuperAdmin: builder.mutation({
+            query: (id) => ({
+                url: `/member/deleteSuperAdmin/${id}`,
+                method: "DELETE"
+            })
+        }),
+        deleteManager: builder.mutation({
+            query: (id) => ({
+                url: `/member/deleteManager/${id}`,
+                method: "DELETE"
+            })
+        }),
+        deleteSeller: builder.mutation({
+            query: (id) => ({
+                url: `/member/deleteSeller/${id}`,
+                method: "DELETE"
+            })
+        }),
+
+    })
+})
+
+export const { useCreateSuperAdminMutation, useCreateSellerMutation, useCreateManagerMutation, useGetAllSuperAdminQuery, useGetAllManagerQuery, useGetAllSellerQuery, useGetSuperAdminQuery, useGetManagerQuery, useGetSellerQuery, useDeleteSuperAdminMutation, useDeleteManagerMutation, useDeleteSellerMutation, useUpdateSuperAdminMutation, useUpdateManagerMutation, useUpdateSellerMutation } = userManagementAPI
